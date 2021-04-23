41da03e317c7c4962d3041b489b064c5fc443008
정현우 <wijeong@gmail.com>
2021-04-23 22:55:29 +0900

    done 01_rockPaperScissors

--- 01_rockPaperScissors/rockPaperScissors.js
+++ 01_rockPaperScissors/rockPaperScissors.js
@@ -1,1 +1,19 @@
 'use strict';
+const rockPaperScissors = function (rounds) {
+    rounds = rounds || 3;
+    let result = [];
+    let plays = ['rock', 'paper', 'scissors'];
+
+    let generate = function(round, play){
+        if(round === 0){
+            result.push(play);
+            return;
+        }
+        for(let i = 0; i < plays.length; i++){
+            let currentPlay = plays[i];
+            generate(round - 1, play.concat(currentPlay));
+        }
+    }
+    generate(rounds, []);
+    return result;
+}
